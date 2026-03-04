3c9b6bab721b555a30bff09c06fb636267930421
BrianZ LNX <brian.zeledon.esquivel@est.una.ac.cr>
2026-03-04 12:03:53 -0600

    Primer commit: subiendo proyecto plantasmon

---
+++ src/screens/userProfile/UserProfile.styles.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+export const userStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        backgroundColor: "rgb(136, 99, 173)",
+    }
+})
---
+++ src/screens/userProfile/UserProfile.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import { userStyles } from "./UserProfile.styles";
+import ScreenWrapper from "@/src/components/screenWrapper/ScreenWrapper";
+
+export default function UserProfile (){
+    return (
+        <ScreenWrapper> 
+            <Text>Screen of uusers</Text>
+        </ScreenWrapper>
+    );
+}
